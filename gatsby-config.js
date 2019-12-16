ca2336bace9a3549e52dd33e4ae1ba8ab9c7da64
Migg88 <miguel141295@hotmail.com>
2019-12-16 18:26:13 -0500

    Add product source

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,13 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
+        path: `${__dirname}/src/products`,
+        name: 'products',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
         path: `${__dirname}/src/pages`,
         name: 'pages',
       },
